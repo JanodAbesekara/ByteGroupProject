0ba0abec5e43d148ac7088c5c0b828b18740a24b
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-02-03 21:16:51 +0530

    edit the nav bar logo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Component/Footer/Footer';
 import Navbar from './Component/Navbar/Navbar';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
    <>
     <Navbar/>
+    <Footer/>
    </>
   );
 }
