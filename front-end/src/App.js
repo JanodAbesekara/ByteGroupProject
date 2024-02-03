60b830d5adabd3131690e4ab13b94a6f5339e28e
buddy <manoiddamalgoda@gmail.com>
2024-02-03 21:43:56 +0530

    Created footer
    Footer Created successfully

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
 import Navbar from './Component/Navbar/Navbar';
-
+import Footer from './Component/Footer/Footer';
 
 
 function App() {
   return (
    <>
     <Navbar/>
+    <Footer/>
    </>
   );
 }
---
+++ front-end/src/Component/Footer/Footer.js
@@ -0,0 +1,67 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+      <div className="Footer">
+        <div className="fbox">
+          <div className="rme">
+            <h2>Send us a message</h2>
+            <br></br>
+            <input
+              className="in1"
+              type="email"
+              name="email"
+              placeholder="Your email"
+            ></input>
+            <br></br>
+            <br></br>
+            <input
+              className="in2"
+              type="text"
+              name="t1"
+              placeholder="Type your message here"
+            ></input>
+            <br></br>
+            <br></br>
+            <button className="seb">Send</button>
+          </div>
+  
+          <div className="lme">
+            <h2>Get Help</h2>
+  
+            <div className="im1">
+              <img src="./Symbles/phone.png" />
+              <h3>+9477-123-456-78</h3>
+            </div>
+            <div className="im2">
+              <img src="./Symbles/gmail.png" />
+              <h3>dreamlearnacademy@gmail.com</h3>
+            </div>
+          </div>
+  
+          <div className="line"></div>
+  
+          <div className="fdown">
+            <div className="li">
+              <h4>@ 2024 DreamLearnAcademy.lk</h4>
+            </div>
+            <div className="logo3">
+              <a href="">
+                <img src="./Symbles/link.png" />
+              </a>
+              <a href="">
+                <img src="./Symbles/fb.png" />
+              </a>
+              <a href="">
+                <img src="./Symbles/inst.png" />
+              </a>
+              <a href="">
+                <img src="./Symbles/yt.png" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
