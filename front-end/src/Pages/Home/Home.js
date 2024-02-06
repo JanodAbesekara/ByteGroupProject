482a9d7765ea5147a20e68364201894e78a22d49
madhuka00 <madhukan040@gmail.com>
2024-02-06 21:21:15 +0530

    Create Home Page

---
+++ front-end/src/Pages/Home/Home.js
@@ -0,0 +1,121 @@
+import React from 'react'
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <div className="imj">
+       
+        <h2>Dream Learn Academy</h2>
+        <p>LoreumIpsumLoreumIpsumLoreumIpsumLoreumIpsumLoreumIpsumIpsum
+           LoreumIpsumLoreumIpsumLoreumIpsumLoreumIpsumLoreumIpsum
+            LoreumIpsumLoreumIpsumLoreumIpsum
+        </p>
+      </div>
+       <div className="new1">
+             <div className="nlg">
+             <img src="./Logos/02.jpg" alt='This is aimage of logo'/>
+             <img src="./Logos/03.jpg.png" alt='This is aimage of logo' />
+             <img src="./Logos/04.jpg" alt='This is aimage of logo'/>
+             <img src="./Logos/05.jpg" alt='This is aimage of logo'/>
+             </div>
+         
+       </div> 
+
+       <div className="filter">
+
+        <h2>Search for a teacher</h2>
+
+      <section className='search'>   
+
+     
+
+
+      
+  
+      <div className="s1">  
+      <label for="subject">Subject</label>
+           <br></br><br></br>
+              <select name="class" className="select">
+                 <option value="">subject</option>
+                 <option value="Mathematics">Mathematics</option>
+                 <option value="Science">Science</option>
+                 <option value="Agriculture">Agriculture</option>
+                 <option value="English Literature">English Literature</option>
+                 <option value="English">English</option>
+                 <option value="ICT">ICT</option>
+                 <option value="Chemistry">Chemistry</option>
+                 <option value="Combined Mathematics">Combined Mathematics</option>
+                 <option value="Physics">Physics</option>
+                 <option value="Biology">Biologye</option>
+                 <option value="Business Studies">Business Studies</option>
+                 <option value="Accounting">Accounting</option>
+                 <option value="Economics">Economics</option>
+                 <option value="Logic and Scientific Method">Logic and Scientific Method</option>
+                 <option value="Political Science">Political Science</option>
+                 <option value="Engineering Technology">Engineering Technology</option>
+                 <option value="Bio Systems Technology">Bio Systems Technology</option>
+                 <option value="Science for Technology">Science for Technology</option>
+              </select>
+         </div> 
+       
+      <div className="s2">
+     
+      <label for="distract">Distract</label>
+        <br></br>
+              <select name="districts" className="select">
+                  <option value="">Select district</option>
+                  <option value="Colombo">Colombo</option>
+                  <option value="Gampaha">Gampaha</option>
+                  <option value="Kandy">Kandy</option>
+                  <option value="Kalutara">Kalutara</option>
+                  <option value="Kurunegala">Kurunegala</option>
+                  <option value="Galle">Galle</option>
+                  <option value="Matara">Matara</option>
+                  <option value="Hambantota">Hambantota</option>
+                  <option value="Jaffna">Jaffna</option>
+                  <option value="Batticaloa">Batticaloa</option>
+                  <option value="Trincomalee">Trincomalee</option>
+                  <option value="Anuradhapura">Anuradhapura</option>
+                  <option value="Polonnaruwa">Polonnaruwa</option>
+                  <option value="Badulla">Badulla</option>
+                  <option value="Monaragala">Monaragala</option>
+                  <option value="Ratnapura">Ratnapura</option>
+                  <option value="Kegalle">Kegalle</option>
+                  <option value="Matale">Matale</option>
+                  <option value="Nuwara Eliya">Nuwara Eliya</option>
+                  <option value="Ampara">Ampara</option>
+                  <option value="Vavuniya">Vavuniya</option>
+                  <option value="Mannar">Mannar</option>
+                  <option value="Mullaitivu">Mullaitivu</option>
+                  <option value="Kilinochchi">Kilinochchi</option>
+              </select>
+       </div>       
+              <br></br>
+
+        <div className="s3">
+             <label for="radiobuttob" className="inputcl">Class Type</label>
+                   <br></br>
+           
+           <ul>
+            <li>
+                 <input type="radio" name="classType" value="individual" /> Individual
+                 
+                 
+            </li>
+                     
+            <li>
+                 <input type="radio" name="classType" value="group" /> Group
+            </li>     
+          </ul>
+    </div>
+       </section> 
+       </div>
+
+
+
+      
+    </div> 
+  )
+}
