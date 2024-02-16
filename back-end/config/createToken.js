ef6e26203863571ab64e4813ddaf9a613a44a845
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-02-16 10:53:17 +0530

    add changes to back end

--- back-end/config/createToken.js
+++ back-end/config/createToken.js
@@ -6,11 +6,8 @@ dotenv.config();
 
 
 const generateToken = (data) => {
-<<<<<<< Updated upstream
     return jwt.sign(data,'$dfdfdsrw34/efefe', { expiresIn: '2d' });
-=======
-    return jwt.sign(data, process.env.SECNAME, { expiresIn: '2d' });
->>>>>>> Stashed changes
+    
 };
 
 export default generateToken;
