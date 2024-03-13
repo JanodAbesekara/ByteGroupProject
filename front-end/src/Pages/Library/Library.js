0c587a5f448cb7c656885f53a88c16334c21d761
buddy <manoiddamalgoda@gmail.com>
2024-03-13 10:51:02 +0530

    edited

---
+++ front-end/src/Pages/Library/Library.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import "./Library.css";
+import Navbar from "../../Component/Navbar/Navbar";
+import Footer from "../../Component/Footer/Footer";
+import TableComponent from "./Component/Tablecomponenet";
+import axios from "axios";
+
+const Library = () => {
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    getFille();
+  }, []);
+
+  const getFille = () => {
+    axios
+      .get(`api/auth/fileurlsend`)
+      .then((response) => {
+        setFiles(response?.data?.data || []);
+        console.log(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="L_label">
+        <h2>Knowledge on your fingertips </h2>
+      </div>
+      <div className="sb1">
+        <TableComponent
+          rows={files.map((file) => ({
+            pdfLink: file.PDFurl,
+            pdfTopic: file.discriP,
+            videoLink: file.videoUrl,
+            videoTopic: file.discriV,
+            audioLink: file.audioUrl,
+            audioTopic: file.audios,
+            pdfSubject: file.pdfS,
+            videoSubject: file.videos,
+            audioSubject: file.audios,
+          }))}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Library;
