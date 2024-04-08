faea91871853e3ff5f40ed6a674abb4bbcea13d1
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-04-08 06:35:03 +0530

    chat app front end

--- front-end/src/Pages/Chat/Chat.js
+++ front-end/src/Pages/Chat/Chat.js
@@ -1,37 +1,41 @@
-
 import "./Chat.css";
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Link } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 function Chat() {
   return (
-    <Box sx={{ flexGrow: 1, height: '100%' }}>
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
-        <Item>
-         <img src="./Componenet/backicon.png" alt="This is back icon"/> 
-
-        </Item>
+    <Box sx={{ flexGrow: 1, height: "100%" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Item>
+            <Link to="">
+              <img
+                width="30"
+                height="30"
+                src="https://img.icons8.com/ios-glyphs/30/circled-left-2.png"
+                alt="circled-left-2"
+              />
+            </Link>
+           
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item></Item>
+        </Grid>
       </Grid>
-      <Grid item xs={8}>
-        <Item>
-          
-
-        </Item>
-      </Grid>
-    </Grid>
-  </Box>
+    </Box>
   );
 }
 export default Chat;
