d4b4639f9509eabe92d2f0078e13e8b1b689deca
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-04-14 06:09:27 +0530

    quizzes display to student part

---
+++ front-end/src/Pages/StudentPages/SQuizzes/ComQuises.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ComQuises() {
+  return (
+    <div>
+      <div style={{float:"right", margin:"50px 100px", boxShadow:"2px 4px 8px 0.5px black",padding:"10px"}}>
+        <p >Time</p>
+      </div>
+    </div>
+  );
+}
+
+export default ComQuises;
--- front-end/src/Pages/StudentPages/SQuizzes/SQuizzes.js
+++ front-end/src/Pages/StudentPages/SQuizzes/SQuizzes.js
@@ -3,6 +3,8 @@ import { Grid, Box } from "@mui/material";
 import Navbar from "../../../Component/Navbar/Navbar";
 import Footer from "../../../Component/Footer/Footer";
 import Ssidebar from "../../../Component/SSidebar/Ssidebar";
+import { Link } from "react-router-dom";
+
 
 function SQuizzes() {
   return (
@@ -13,7 +15,11 @@ function SQuizzes() {
           <Ssidebar />
         </Grid>
         <Grid item md={11.25} sm={10.5} xs={9.8}>
-          <Box sx={{ width: "100%", height: "1000px" }}></Box>
+          <Box sx={{display:"flex",justifyContent:"center",alignContent:"center"}}>
+            <div style={{width:"80%", height:"60%",backgroundColor:"#C3B091", padding:"40px", borderRadius:"20px" , marginTop:"40px",marginBottom:"30px", boxShadow:"2px 4px 8px 0.5px black"}}>
+              <Link to="/ComQuises"><button style={{float:"right", padding:"5px 10px",boxShadow:"2px 1px 10px 0.5px black"}}> Attempet to Quise</button></Link>
+            </div>
+          </Box>
         </Grid>
       </Grid>
       <Footer />
