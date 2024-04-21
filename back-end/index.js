9c815968cfce36b150783895f54d0a750edebcf1
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-04-21 08:01:49 +0530

    new edit

--- back-end/index.js
+++ back-end/index.js
@@ -8,13 +8,11 @@ import authAPI from "./apis/authAPI.js";
 import emailAPI from "./apis/emailAPI.js";
 // import chatAPI from "./apis/ChatAPI.js";
 import userAPI from "./apis/userAPI.js";
-import {app, io, server} from "./Socket-io/Socketiosever.js";
+import { app, io, server } from "./Socket-io/Socketiosever.js";
 import QuiseAPI from "./apis/QuiseAPI.js";
 import EnrolAPI from "./apis/EnrolAPI.js";
 
-
-
-// Connection to the database 
+// Connection to the database
 dbConnect();
 
 // Middleware
@@ -34,16 +32,8 @@ app.use("/api/email", emailAPI);
 // app.use("/api/chat", chatAPI);
 app.use("/api/user", userAPI);
 app.use("/api/Quise", QuiseAPI);
-app.use("/api/Enrol",  EnrolAPI);
-
+app.use("/api/Enrol", EnrolAPI);
 
 // Port
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server port ${port}`));
-
-
-
- 
-
-
-
