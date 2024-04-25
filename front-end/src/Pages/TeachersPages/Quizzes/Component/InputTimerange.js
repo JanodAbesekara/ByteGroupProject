ee65e9135e01319d88a07ae351e0e80b7c5ec098
buddy <manoiddamalgoda@gmail.com>
2024-04-25 12:59:31 +0530

    edited quizzes UI
    quizzes creating page UIs edited

--- front-end/src/Pages/TeachersPages/Quizzes/Component/InputTimerange.js
+++ front-end/src/Pages/TeachersPages/Quizzes/Component/InputTimerange.js
@@ -7,21 +7,29 @@ function InputTimerange({ setTimeRange ,setQuizNumber}) {
   const [quizNumber, setQuizNumberLocal] = useState(0);
 
   const handleTimeRangeChange = (e) => {
+    setDisable1(false);
     setTimeRangeLocal(e.target.value);
     setTimeRange(e.target.value); // Update parent state
   };
 
   const handleQuizNumberChange = (e) => {
+    setDisable2(false);
     setQuizNumberLocal(e.target.value);
     setQuizNumber(e.target.value); // Update parent state
   };
 
+  const [disable1,setDisable1] = useState(true);
+  const [disable2,setDisable2] = useState(true);
 
   return (
     <div>
-      <label>Time Range: </label>
-      <select onChange={handleTimeRangeChange} value={timeRange}>
-        <option value={0}>Select Time</option>
+      <label style={{color: "#232424"}}>Time Range: </label>
+
+      <select style={{
+        borderRadius: "5px",
+        color: "#4f5251"
+      }}onChange={handleTimeRangeChange} value={timeRange} >
+        {disable1 && <option>Select Time</option> }
         <option value={20}>20 minutes</option>
         <option value={30}>30 minutes</option>
         <option value={45}>45 minutes</option>
@@ -29,9 +37,13 @@ function InputTimerange({ setTimeRange ,setQuizNumber}) {
         <option value={90}>90 minutes</option>
       </select>
       <br/><br/>
-      <label>Select Quizzes: </label>
-      <select onChange={handleQuizNumberChange} value={quizNumber}>
-        <option value={0}>Select Quizzes</option>
+      <label style={{color: "#232424"}}>Select Quizzes: </label>
+      <select 
+      style={{
+        borderRadius: "5px",
+        color: "#4f5251"
+      }}onChange={handleQuizNumberChange} value={quizNumber}>
+      {disable2 && <option>Select Quizzes</option> }
         <option value="1">Quizzes 1</option>
         <option value="2">Quizzes 2</option>
         <option value="3">Quizzes 3</option>
