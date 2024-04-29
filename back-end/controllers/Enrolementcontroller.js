8703d3e5af72cf701602c9e77a37d3173c2d4d4c
buddy <manoiddamalgoda@gmail.com>
2024-04-29 19:47:39 +0530

    Revert "Merge branch 'main' into manoj-01"
    This reverts commit 4fb459fb7fa3e90f0b9843f374e227cccb477aa1, reversing
    changes made to 1ac8028ff622f41c71ee9fba62385c591a3ce42b.

--- back-end/controllers/Enrolementcontroller.js
+++ back-end/controllers/Enrolementcontroller.js
@@ -41,12 +41,11 @@ const Enrolementcontroller = async (req, res) => {
 };
 
 const StudentEnrollment = async (req, res) => {
-  const { userEmail, teacherEmail, Ensubject, Enmedium } = req.body;
+  const { userEmail, teacherEmail, Ensubject ,Enmedium } = req.body;
 
-  if (userEmail == null) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "Please Register to the Site" });
+
+  if(userEmail == null){
+    return res.status(400).json({ success: false, msg: "Please Register to the Site" });
   }
 
   if (!userEmail || !teacherEmail || !Ensubject || !Enmedium) {
@@ -108,33 +107,15 @@ const StudentEnrollment = async (req, res) => {
   }
 };
 
+
 const getSubjects = async (req, res) => {
   try {
     const subjects = await EnrollmentSchema.find();
     return res.status(200).json({ success: true, data: subjects });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, msg: "Internal Server Error" });
-  }
-};
-
-const getstudentregistedteachers = async (req, res) => {
-  try {
-   
-    const enrolteacher = await EnrollmentSchema.find();
-
-     return res.status(200).json({ success: true, data: enrolteacher });
-   
   } catch (error) {
     return res.status(500).json({ success: false, msg: "Internal Server Error" });
   }
 };
 
 
-export {
-  Enrolementcontroller,
-  StudentEnrollment,
-  getSubjects,
-  getstudentregistedteachers,
-};
+export { Enrolementcontroller, StudentEnrollment ,getSubjects };
--- front-end/src/Pages/StudentPages/STeachers/STeachers.js
+++ front-end/src/Pages/StudentPages/STeachers/STeachers.js
@@ -1,45 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Grid, Box } from "@mui/material";
 import Navbar from "../../../Component/Navbar/Navbar";
 import Footer from "../../../Component/Footer/Footer";
 import Ssidebar from "../../../Component/SSidebar/Ssidebar";
-import Axios from "axios";
-import {
-  TableContainer,
-  Paper,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-} from "@mui/material";
-import { jwtDecode } from "jwt-decode";
 
 function STeachers() {
-  const [subjectdetal, setsubjectdetal] = useState([]);
-
-  useEffect(() => {
-    const featchsubject = async () => {
-      const token = localStorage.getItem("MERN_AUTH_TOKEN");
-      const decodedToken = jwtDecode(token);
-      const Stuemail = decodedToken.email;
-
-      try {
-        const response = await Axios.get(`/api/Enrol/getteacher`);
-
-        const data = response.data.data.filter(
-          (item) => item.userEmail === Stuemail
-        );
-        console.log(data);
-        setsubjectdetal(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    featchsubject();
-  }, []);
-
   return (
     <div>
       <Navbar />
@@ -48,63 +13,7 @@ function STeachers() {
           <Ssidebar />
         </Grid>
         <Grid item md={11.25} sm={10.5} xs={9.8}>
-          <Box>
-            <h1>Enrolled Teachers</h1>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell
-                      sx={{
-                        textAlign: "center",
-                        backgroundColor: "#317873",
-                        borderRight: "2px white solid",
-                        color: "white",
-                      }}
-                    >
-                      Email
-                    </TableCell>
-                    <TableCell
-                      sx={{
-                        textAlign: "center",
-                        backgroundColor: "#317873",
-                        borderRight: "2px white solid",
-                        color: "white",
-                      }}
-                    >
-                      Subject
-                    </TableCell>
-                    <TableCell
-                      sx={{
-                        textAlign: "center",
-                        backgroundColor: "#317873",
-                        borderRight: "2px white solid",
-                        color: "white",
-                      }}
-                    >
-                      Medium
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {subjectdetal.length > 0 &&
-                    subjectdetal.map((subjectDetails, index) => (
-                      <TableRow key={index}>
-                        <TableCell sx={{ textAlign: "center" }}>
-                          {subjectDetails.teacherEmail}
-                        </TableCell>
-                        <TableCell sx={{ textAlign: "center" }}>
-                          {subjectDetails.Ensubject}
-                        </TableCell>
-                        <TableCell sx={{ textAlign: "center" }}>
-                          {subjectDetails.Enmedium}
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Box>
+          <Box sx={{ width: "100%", height: "1000px" }}></Box>
         </Grid>
       </Grid>
       <Footer />
