9847a1cd83856154233b998e6b966f33a4a8ff59
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-05-27 23:20:05 +0530

    new chages of attendece part

--- front-end/src/Pages/StudentPages/SSubject/AttendanceChart.js
+++ front-end/src/Pages/StudentPages/SSubject/AttendanceChart.js
@@ -3,10 +3,21 @@ import Stack from '@mui/material/Stack';
 import { Gauge } from '@mui/x-charts/Gauge';
 
 export default function BasicGauges({value}) {
+  let color;
+  
+  if (value < 20) {
+    color = 'red';
+  } else if ( value <= 50) {
+    color = 'yellow';
+  } else if (value <= 80) {
+    color = 'blue';
+  }else{
+    color = 'green';
+  }
   
   return (
     <Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 1, md: 3 }}>
-      <Gauge width={100} height={100} value={value} /> 
+      <Gauge width={100} height={100} value={value} color={color} /> 
     </Stack>
   );
 }
