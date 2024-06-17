bc78020992e7a308cadc5683b88bf706c3885ba6
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-17 11:34:48 +0530

    fix problewm

--- front-end/src/Pages/StudentPages/Payment/PaymentdisplayTable.js
+++ front-end/src/Pages/StudentPages/Payment/PaymentdisplayTable.js
@@ -13,19 +13,19 @@ import axios from "axios";
 
 function PaymentdisplayTable() {
   const [PaymentData, setPaymentData] = useState([]);
-  const [visible,setVisible] = useState(false);
-
+ 
   useEffect(() => {
     const fetchData = async () => {
       const token = localStorage.getItem("MERN_AUTH_TOKEN");
       const decodedToken = jwtDecode(token);
       const email = decodedToken.email;
       try {
-        const response = await axios.get("/api/Test/getdetails", {
+        const response = await axios.get("/api/Test/getdertails", {
           params: { email },
         });
         setPaymentData(response.data.data);
-        setVisible(response.data.success);
+        console.log("details"+response.data.data);
+        
       } catch (error) {
         window.alert(error.response ? error.response.data.msg : error.message);
       }
@@ -83,7 +83,7 @@ function PaymentdisplayTable() {
           </TableBody>
         </Table>
       </TableContainer>
-      )}
+      
     </div>
   );
 }
