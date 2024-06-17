de9b350d5e49c380a3906bb6ea919e6b6a457654
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-17 17:18:08 +0530

    Update

--- back-end/Socket-io/Socketiosever.js
+++ back-end/Socket-io/Socketiosever.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://bytegroupproject.onrender.com", "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
