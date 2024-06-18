fd2a2d231074e65163f5a75ea4cfe4a62e9a9d45
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-18 19:51:43 +0530

    Update ComQuises1.js

--- front-end/src/Pages/StudentPages/SQuizzes/ComQuises1.js
+++ front-end/src/Pages/StudentPages/SQuizzes/ComQuises1.js
@@ -30,7 +30,7 @@ function ComQuizes({ quisedata }) {
       handleSubmit(); // Automatically submit when time is over
     }
     return () => clearInterval(timer);
-  }, [countdownStarted, remainingTime ]);
+  }, [countdownStarted, remainingTime]);
 
   const startQuiz = async () => {
     const timeRangeFromBackend = quisedata.TimeRanges;
@@ -50,9 +50,7 @@ function ComQuizes({ quisedata }) {
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
-
   const handleSubmit = () => {
-
     setSubmitButton(true);
     const score = checkAnswers();
     // window.alert(`Your score is ${score} %`);
@@ -69,7 +67,6 @@ function ComQuizes({ quisedata }) {
     console.log("first");
   };
 
-
   const checkAnswers = () => {
     let score = 0;
     for (let i = 0; i < correctAnswers.length; i++) {
@@ -79,7 +76,6 @@ function ComQuizes({ quisedata }) {
     }
     return (score / correctAnswers.length) * 100;
   };
-
 
   // Function to handle next question
   const nextQuestion = () => {
@@ -94,146 +90,164 @@ function ComQuizes({ quisedata }) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
     }
 
-  const resetNotification = () => {
-    setTriggerNotification(false);
-    setCountdownStarted(false);
-    setShowContent(false);
+    const resetNotification = () => {
+      setTriggerNotification(false);
+      setCountdownStarted(false);
+      setShowContent(false);
+    };
 
-  };
+    return (
+      <>
+        <div>
+          {Alertdata.show && (
+            <AlertBox
+              data={Alertdata}
+              triggerNotification={triggerNotification}
+              resetNotification={resetNotification}
+            />
+          )}
+          {!quizStarted && (
+            <button
+              onClick={() => setShowContent(!showContent)}
+              style={{ marginTop: "10px", marginLeft: "5px" }}
+            >
+              {showContent ? "Hide Content" : "Show Content"}
+            </button>
+          )}
 
-  return (
-    <>
-
-      <div>
-    {Alertdata.show && (
-          <AlertBox
-            data={Alertdata}
-            triggerNotification={triggerNotification}
-            resetNotification={resetNotification}
-          />
-        )}
-        {!quizStarted && (
-        <button onClick={() => setShowContent(!showContent)} style={{ marginTop: "10px", marginLeft: "5px" }}>
-
-          {showContent ? "Hide Content" : "Show Content"}
-        </button>
-        )}
-
-        {showContent && (
-
-          <div
-            style={{
-              backgroundColor: "#fff",
-              padding: "35px",
-              border: "none",
-              borderRadius: "6px",
-              color: "#000",
-              width: "80%",
-              margin: "3% 10%",
-            }}
-          >
-            {!quizStarted && ( // Render start button if quiz has not started
-              <button onClick={startQuiz} style={{ marginTop: "3px" }}>Start Quiz</button>
-            )}
-
-            <div>
-              {showContent && quizStarted && (
-                <p
-                  style={{
-                    float: "right",
-                    backgroundColor: "#ada9a8",
-                    color: "#000",
-                    border: "none",
-                    borderRadius: "5px",
-                    padding: "4px",
-                    marginBottom: "10px",
-                  }}
-                >
-                  Remaining Time: <span style={{ color: "red" }}>{formatTime()}</span>
-                </p>
+          {showContent && (
+            <div
+              style={{
+                backgroundColor: "#fff",
+                padding: "35px",
+                border: "none",
+                borderRadius: "6px",
+                color: "#000",
+                width: "80%",
+                margin: "3% 10%",
+              }}
+            >
+              {!quizStarted && ( // Render start button if quiz has not started
+                <button onClick={startQuiz} style={{ marginTop: "3px" }}>
+                  Start Quiz
+                </button>
               )}
 
-              {quizStarted && ( // Render quiz content if quiz has started
-                <form onSubmit={(e) => { e.preventDefault(); }}>
-                  <div
-                    key={currentQuestionIndex}
+              <div>
+                {showContent && quizStarted && (
+                  <p
                     style={{
-                      paddingTop: "40px",
+                      float: "right",
+                      backgroundColor: "#ada9a8",
+                      color: "#000",
+                      border: "none",
+                      borderRadius: "5px",
+                      padding: "4px",
+                      marginBottom: "10px",
                     }}
                   >
-                    <p>
-                      {currentQuestionIndex + 1}. {quisedata.question[currentQuestionIndex].Question}
-                    </p>
-                    <ul style={{ marginTop: "20px", marginBottom: "20px" }}>
-                      {quisedata.question[currentQuestionIndex].answers.map((answer, aIndex) => (
-                        <li key={aIndex} style={{ margin: "10px" }}>
-                          <input
-                            type="radio"
-                            id={`answer_${currentQuestionIndex}_${aIndex}`}
-                            name={`answer_${currentQuestionIndex}`}
-                            value={aIndex}
-                            onChange={() => {
-                              const newAnswers = [...answers];
-                              newAnswers[currentQuestionIndex] = aIndex;
-                              setAnswers(newAnswers);
-                            }}
-                            checked={answers[currentQuestionIndex] === aIndex}
-                          />
-                          <label htmlFor={`answer_${currentQuestionIndex}_${aIndex}`}>
-                            {answer}
-                          </label>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
+                    Remaining Time:{" "}
+                    <span style={{ color: "red" }}>{formatTime()}</span>
+                  </p>
+                )}
 
-                  {/* Navigation buttons */}
-                  <div style={{ display: "flex", justifyContent: "center" }}>
-                    <button
-                      type="button"
-                      onClick={prevQuestion}
-                      disabled={currentQuestionIndex === 0}
+                {quizStarted && ( // Render quiz content if quiz has started
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                    }}
+                  >
+                    <div
+                      key={currentQuestionIndex}
                       style={{
-                        margin: "4px",
-                        padding: "2px",
+                        paddingTop: "40px",
                       }}
                     >
-                      {"<"}
-                    </button>
+                      <p>
+                        {currentQuestionIndex + 1}.{" "}
+                        {quisedata.question[currentQuestionIndex].Question}
+                      </p>
+                      <ul style={{ marginTop: "20px", marginBottom: "20px" }}>
+                        {quisedata.question[currentQuestionIndex].answers.map(
+                          (answer, aIndex) => (
+                            <li key={aIndex} style={{ margin: "10px" }}>
+                              <input
+                                type="radio"
+                                id={`answer_${currentQuestionIndex}_${aIndex}`}
+                                name={`answer_${currentQuestionIndex}`}
+                                value={aIndex}
+                                onChange={() => {
+                                  const newAnswers = [...answers];
+                                  newAnswers[currentQuestionIndex] = aIndex;
+                                  setAnswers(newAnswers);
+                                }}
+                                checked={
+                                  answers[currentQuestionIndex] === aIndex
+                                }
+                              />
+                              <label
+                                htmlFor={`answer_${currentQuestionIndex}_${aIndex}`}
+                              >
+                                {answer}
+                              </label>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </div>
+
+                    {/* Navigation buttons */}
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                      <button
+                        type="button"
+                        onClick={prevQuestion}
+                        disabled={currentQuestionIndex === 0}
+                        style={{
+                          margin: "4px",
+                          padding: "2px",
+                        }}
+                      >
+                        {"<"}
+                      </button>
+                      <button
+                        type="button"
+                        onClick={nextQuestion}
+                        disabled={
+                          currentQuestionIndex === quisedata.question.length - 1
+                        }
+                        style={{
+                          margin: "4px",
+                          padding: "2px",
+                        }}
+                      >
+                        {">"}
+                      </button>
+                    </div>
                     <button
-                      type="button"
-                      onClick={nextQuestion}
-                      disabled={currentQuestionIndex === quisedata.question.length - 1}
+                      type="submit"
+                      disabled={!submitButton || remainingTime === 0}
+                      onClick={handleSubmit}
                       style={{
-                        margin: "4px",
-                        padding: "2px",
+                        width: "auto",
+                        margin: "3px",
+                        padding: "3px",
+                        border: "none",
+                        borderRadius: "5px",
+                        backgroundColor: "#103457",
+                        color: "#fff",
+                        boxShadow: "0px 2px 2px 0px",
                       }}
                     >
-                      {">"}
+                      Submit
                     </button>
-                  </div>
-                  <button type="submit" disabled={!submitButton || remainingTime === 0} onClick={handleSubmit}
-                          style={{
-                            width:"auto",
-                            margin: "3px",
-                            padding: "3px",
-                            border: "none",
-                            borderRadius: "5px",
-                            backgroundColor: "#103457",
-                            color: "#fff",
-                            boxShadow: "0px 2px 2px 0px",
-                          }}>
-                    Submit
-                  </button>
-                </form>
-              )}
+                  </form>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+          )}
+        </div>
+      </>
+    );
+  };
 }
-
 export default ComQuizes;
-
