e2f992f97abe906d132fe8358e339da668d0d59e
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-20 19:46:39 +0530

    edit

--- back-end/config/createToken.js
+++ back-end/config/createToken.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const generateToken = (data) => {
-  return jwt.sign(data,process.env.JWT_SECRET, { expiresIn: "1min" });
+  return jwt.sign(data,process.env.JWT_SECRET, { expiresIn: "5min" });
 };
 
 export default generateToken;
