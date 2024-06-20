4fd2241ab31706301289f845306c0f7f7e63cae1
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-20 20:07:44 +0530

    new changes

--- front-end/src/Pages/StudentPages/SGrades/SGrades.js
+++ front-end/src/Pages/StudentPages/SGrades/SGrades.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Box } from "@mui/material";
 import Navbar from "../../../Component/Navbar/Navbar";
 import Footer from "../../../Component/Footer/Footer";
@@ -15,38 +15,36 @@ import {
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-
-
 function SGrades() {
-
-  const [grades,setGrades] = useState([]);
+  const [grades, setGrades] = useState([]);
 
   useEffect(() => {
     const token = localStorage.getItem("MERN_AUTH_TOKEN");
-        const decodedToken = jwtDecode(token);
-        const userEmail = decodedToken.email;
+    const decodedToken = jwtDecode(token);
+    const userEmail = decodedToken.email;
 
-    axios.get(`api/assignment/getGrade`,{params :{ email:userEmail }})
-         .then((response) => {
-          setGrades(response.data);
-         })
-         .catch((error) => {
-          console.log(error.response.data);
-         })
-  },[]);
+    axios
+      .get(`api/assignment/getGrade`, { params: { email: userEmail } })
+      .then((response) => {
+        setGrades(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }, []);
 
   const getBackgroundColor = (grade) => {
-    if (grade === 'W') {
+    if (grade === "W") {
       return "#ff0000"; // red
-    } else if (grade ==='D') {
+    } else if (grade === "D") {
       return "#ff8000"; // orange
-    } else if (grade ==='C') {
+    } else if (grade === "C") {
       return "#ffff00"; // yellow
-    } else if (grade ==='B') {
+    } else if (grade === "B") {
       return "#80ff00"; // light green
-    } else if (grade ==='A') {
+    } else if (grade === "A") {
       return "#00ff00"; // green
-    }else{
+    } else {
       return "#ffffff"; // white
     }
   };
@@ -60,60 +58,80 @@ function SGrades() {
         </Grid>
         <Grid item md={11.25} sm={10.5} xs={9.8}>
           <Box>
-          <TableContainer component={Paper} sx={{ margin: "50px 0px" }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell
-                sx={{
-                  textAlign: "center",
-                  backgroundColor: "#317873",
-                  borderRight: "2px white solid",
-                  color: "white",
-                }}
-              >
-                Subject
-              </TableCell>
-              <TableCell
-                sx={{
-                  textAlign: "center",
-                  backgroundColor: "#317873",
-                  borderRight: "2px white solid",
-                  color: "white",
-                }}
-              >
-                Medium
-              </TableCell>
+            <TableContainer component={Paper} sx={{ margin: "50px 0px" }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      sx={{
+                        textAlign: "center",
+                        backgroundColor: "#317873",
+                        borderRight: "2px white solid",
+                        color: "white",
+                      }}
+                    >
+                      Teacher Email
+                    </TableCell>
+                    <TableCell
+                      sx={{
+                        textAlign: "center",
+                        backgroundColor: "#317873",
+                        borderRight: "2px white solid",
+                        color: "white",
+                      }}
+                    >
+                      Subject
+                    </TableCell>
+                    <TableCell
+                      sx={{
+                        textAlign: "center",
+                        backgroundColor: "#317873",
+                        borderRight: "2px white solid",
+                        color: "white",
+                      }}
+                    >
+                      Medium
+                    </TableCell>
 
-              <TableCell
-                sx={{
-                  textAlign: "center",
-                  backgroundColor: "#317873",
-                  borderRight: "2px white solid",
-                  color: "white",
-                }}
-              >
-                Grade
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {grades.map((eachOne) => (
-                <TableRow>
-                  <TableCell sx={{ textAlign: "center" }}>
-                    {eachOne.subject}
-                  </TableCell>
-                  <TableCell sx={{ textAlign: "center" }}>
-                    {eachOne.medium}
-                  </TableCell>
-                  <TableCell sx={{ textAlign: "center", backgroundColor: getBackgroundColor(eachOne.grade), }}>
-                    {eachOne.grade}
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                    <TableCell
+                      sx={{
+                        textAlign: "center",
+                        backgroundColor: "#317873",
+                        borderRight: "2px white solid",
+                        color: "white",
+                      }}
+                    >
+                      Grade
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {grades.map((eachOne) => (
+                    <TableRow>
+                      <TableCell sx={{ textAlign: "center" }}>
+                        <a href={`mailto:${eachOne.teacherEmail}`}>
+                          {eachOne.teacherEmail}
+                        </a>
+                      </TableCell>
+                      <TableCell sx={{ textAlign: "center" }}>
+                        {eachOne.subject}
+                      </TableCell>
+                      <TableCell sx={{ textAlign: "center" }}>
+                        {eachOne.medium}
+                      </TableCell>
+                      <TableCell
+                        sx={{
+                          textAlign: "center",
+                          backgroundColor: getBackgroundColor(eachOne.grade),
+                        }}
+                      >
+                        {eachOne.grade}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Box>
         </Grid>
       </Grid>
