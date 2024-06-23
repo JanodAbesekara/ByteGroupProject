d4db189a4abfe155302af2cf36dcd644dd1cef71
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-23 10:35:10 +0530

    Update StudentpayemtStates.js

--- back-end/models/StudentpayemtStates.js
+++ back-end/models/StudentpayemtStates.js
@@ -32,6 +32,11 @@ const StudentpaymentStatesSchema = new Schema(
       type: String,
       required: true,
     },
+    createdAt: { 
+      type: Date, 
+      default: Date.now, 
+      expires: '20d'  // TTL index to expire documents after 20 days
+    }
   },
   {
     timestamps: true,
