eb9dd408495da2a765b6e435c1b47ee96fe8fae6
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-06-23 06:50:05 +0530

    Update sendEmails.js

--- back-end/config/sendEmails.js
+++ back-end/config/sendEmails.js
@@ -23,19 +23,36 @@ const sendVerificationEmail = async (senderAddress, link) => {
       <!DOCTYPE html>
       <html>
         <head>
-          <title>Reset Your password</title>
+          <title>Verify Email!</title>
          
         </head>
         <style>
+        h2{
+          text-align: center;
+          color: blue;
+        }
+          .HQ{
+            justify-content: center;
+            align-content: center;
+            display: flex;
+          }
+          button{
+            color: white;
+            background-color: green;
+            border: none;
+            padding: 4px 8px;
+            box-shadow: 1px 1px 4px 0.5px black;
+            cursor: pointer;
+          }
           button:visite{
             color:yellow;
             background-color: blue;
           }
         </style>
         <body>
-          <h2 style="text-align:center;color:blue">Click to Reset your Password</h2>
-          <div class="HQ" style="justify-content:center; align-content:center;display:flex;">
-           <a href="${link}"> <button style="color:white;background-color:green;border:none;padding: 4px 8px;box-shadow:1px 1px 4px 0.5px black;cursor: pointer;">Reset</button></a>
+          <h2>Click the button to Verify your account</h2>
+          <div class="HQ">
+           <a href="${link}"> <button>Verify</button></a>
           </div>
         </body>
       </html>
@@ -57,43 +74,25 @@ const sendForgotPasswordEmail = async (senderAddress, link) => {
       to: senderAddress,
       subject: "Reset Password",
       html: `
-      
       <!DOCTYPE html>
-<html>
-  <head>
-    <title>Reset Your password</title>
-   
-  </head>
-  <style>
-  h2{
-    text-align: center;
-    color: blue;
-  }
-    .HQ{
-      justify-content: center;
-      align-content: center;
-      display: flex;
-    }
-    button{
-      color: white;
-      background-color: green;
-      border: none;
-      padding: 4px 8px;
-      box-shadow: 1px 1px 4px 0.5px black;
-      cursor: pointer;
-    }
-    button:visite{
-      color:yellow;
-      background-color: blue;
-    }
-  </style>
-  <body>
-    <h2>Click to Reset your Password</h2>
-    <div class="HQ">
-    <a href="${link}"> <button>Reset</button></a>
-    </div>
-  </body>
-</html>
+      <html>
+        <head>
+          <title>Reset Your password</title>
+         
+        </head>
+        <style>
+          button:visite{
+            color:yellow;
+            background-color: blue;
+          }
+        </style>
+        <body>
+          <h2 style="text-align:center;color:blue">Click to Reset your Password</h2>
+          <div class="HQ" style="justify-content:center; align-content:center;display:flex;">
+           <a href="${link}"> <button style="color:white;background-color:green;border:none;padding: 4px 8px;box-shadow:1px 1px 4px 0.5px black;cursor: pointer;">Reset</button></a>
+          </div>
+        </body>
+      </html>
       `,
     });
   } catch (e) {
